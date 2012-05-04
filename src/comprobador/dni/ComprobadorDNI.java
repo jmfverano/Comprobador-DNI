9cab1f292229ee41ebbf4f40a981a142ba0e5180
Jose Manuel <josemanuel.tecnico@gmai.com>
2012-05-04 17:06:10 +0200

    Creado programa comprobación DNI.

---
+++ src/comprobador/dni/ComprobadorDNI.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comprobador.dni;
+
+/**
+ *
+ * @author jmfverano
+ */
+public class ComprobadorDNI {
+
+    private Principal mi_ventana;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Principal mi_ventana = new Principal();
+        
+    }
+}
